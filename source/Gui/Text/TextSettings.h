05a975f4680e5528066e856675fa2c5d04c5d972
till <tillplusplus@gmail.com>
2022-03-10 20:13:14 +0100

    Text BackgroundColour

--- source/Gui/Text/TextSettings.h
+++ source/Gui/Text/TextSettings.h
@@ -10,7 +10,8 @@ public:
 	int x=0,y=0;
 
 	Font* font = nullptr;
-	glm::vec4 textColor = glm::vec4(0,1,0,1);
+	glm::vec4 textColor = glm::vec4(1,1,1,1);
+	glm::vec4 backgroundColor = glm::vec4(0,0,0,1);
 	bool bold = true,italic = false;//underline = false;
 	// int &fontSize;
 	// int &baseline;
--- source/Gui/Text/TextUnit.cpp
+++ source/Gui/Text/TextUnit.cpp
@@ -15,13 +15,31 @@ void TextUnit::draw(Shader &shader, TextSettings &ts)
 	if (!ts.font)
 		Log::info("No font used.");
 
+	
+	//draw background
+	{
+		Texture::whiteTexture().use(0,shader,"texture1");
+		glUniform4f(glGetUniformLocation(shader.getOpenGLID(), "textureRect"), 0,0,1,1);
+		glUniform4f(glGetUniformLocation(shader.getOpenGLID(), "color"), ts.backgroundColor.r,ts.backgroundColor.g,ts.backgroundColor.b,ts.backgroundColor.a);
+		
+
+		GLfloat ypos =  ts.y;
+		GLfloat xpos = ts.x;
+		GLfloat height = ts.font->getLineSpacing();
+		GLfloat width = getWidth(ts);
+
+		glm::mat4 letterMatrix = glm::mat4(1.f);
+		letterMatrix = glm::translate(letterMatrix, glm::vec3(xpos, ypos+height, 0.f));
+		letterMatrix = glm::scale(letterMatrix, glm::vec3(width, -height, 1.f));
+		
+		Mesh::rectangle().draw(shader, /*inMatrix * matrix */ letterMatrix);
+	}
 	//draw Text
 	glUniform4f(glGetUniformLocation(shader.getOpenGLID(), "color"), ts.textColor.r,ts.textColor.g,ts.textColor.b,ts.textColor.a);
 	ts.font->use(shader);
 
 	auto &line = text;
 	GLfloat offset = 0;
-
 	for (size_t i = 0; i < text.getSize(); i++)
 	{
 		//tab
@@ -47,15 +65,15 @@ void TextUnit::draw(Shader &shader, TextSettings &ts)
 		//draw letter
 		glUniform4fv(glGetUniformLocation(shader.getOpenGLID(), "textureRect"), 1, glm::value_ptr(ts.font->getRelativTextureRectGlyph(glyph)));
 
-		GLfloat ypos = glyph.bounds.top + ts.font->getBaseline() + ts.font->getLineSpacing();
+		GLfloat ypos = glyph.bounds.top + ts.font->getBaseline() + ts.font->getLineSpacing() +ts.y;
 		GLfloat xpos = glyph.bounds.left + offset +ts.x;
 		GLfloat height = glyph.bounds.height;
 		GLfloat width = glyph.bounds.width;
 
 		glm::mat4 letterMatrix = glm::mat4(1.f);
-		if(ts.italic)
-			letterMatrix[1] = {-0.15,1,0,0};//change for italic
 		letterMatrix = glm::translate(letterMatrix, glm::vec3(xpos, ypos+height, 0.f));
+		if(ts.italic)
+			letterMatrix[1] += glm::vec4(-0.15,0,0,0);//change for italic
 		letterMatrix = glm::scale(letterMatrix, glm::vec3(width, -height, 1.f));
 		
 		Mesh::rectangle().draw(shader, /*inMatrix * matrix */ letterMatrix);
@@ -64,7 +82,7 @@ void TextUnit::draw(Shader &shader, TextSettings &ts)
 		offset += glyph.advance;
 	}
 }
-int TextUnit::getWidth(TextSettings &ts){
+int TextUnit::getWidth(const TextSettings &ts){
 	if (!ts.font)
 		Log::info("No font used.");
 
@@ -98,6 +116,12 @@ int TextUnit::getWidth(TextSettings &ts){
 		offset += glyph.advance;
 	}
 	return offset;
+}
+int TextUnit::getHeight(const TextSettings &ts){
+	return ts.font->getLineSpacing();
+}
+int TextUnit::getYOffset(const TextSettings &ts){
+	return ts.font->getBaseline();
 }
 std::string TextUnit::getType()
 {
